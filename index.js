599f5aea142e0498f5b84495dbb205ca94c3cc66
ChristianCudby <christiancudby@hotmail.com>
2021-04-15 04:16:38 -0400

    commit tree f879a21b70194712a9e99a3d014a119153a5c1da

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+document.head.appendChild(newHeader)
+newHeader.innerHTML = 'YOUR NAME is the champion'
